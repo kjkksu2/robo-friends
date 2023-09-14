527469475e921125a0dd406faa540e676cac51ec
Kim Seok Yoon <kjkksu2@naver.com>
2023-09-14 11:20:19 +0900

    class component

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => this.setState({ robots: json }))
+      .catch((err) => console.log(err));
+  }
+
+  searchHandler = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    }
+
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox onSearch={this.searchHandler} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
